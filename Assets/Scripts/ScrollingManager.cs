820ba949fb865197433831b477a7086aad22fceb
Erocal <s159752zxcvbn1@gmail.com>
2024-12-15 02:45:24 +0800

    [!][-][+]升級成FullScreen

---
+++ Assets/Scripts/ScrollingManager.cs
@@ -0,0 +1,113 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingManager : MonoBehaviour
+{
+
+    #region -- 資源參考區 --
+
+    [Header("Script")]
+    public ScrollingText scrollingText;
+
+    [Header("TMPro")]
+    public TMP_InputField InputField_Text_Color_R;
+    public TMP_InputField InputField_Text_Color_G;
+    public TMP_InputField InputField_Text_Color_B;
+    public TMP_InputField InputField_Text_Color_A;
+    public TMP_InputField InputField_Text_Speed;
+
+    [Header("GameObject")]
+    public GameObject Scrolling_Text_Group;
+    public GameObject Settings;
+
+    [Header("Button")]
+    public Button button_Return;
+
+    [Header("Material")]
+    public Material fullScreen_ScrollingText;
+
+    #endregion
+
+    void Awake()
+    {
+        // 防呆檢查：確保面板物件已設置
+        if (Scrolling_Text_Group == null)
+        {
+            Debug.LogError("scrollingText未設置。請在檢視器中拖曳對應的面板物件。");
+            enabled = false; // 禁用此腳本以防止錯誤操作
+            return;
+        }
+
+        if (button_Return != null)
+        {
+            button_Return.onClick.AddListener(() => ActiveScrollingTextUI(true));
+        }
+
+    }
+
+    void Update()
+    {
+
+        SetScrollSpeed();
+        SetTextColor();
+
+        // 檢測PC上的滑鼠點擊事件
+        if (Input.GetMouseButtonDown(0))
+        {
+            ActiveScrollingTextUI(false);
+        }
+
+        // 檢測手機上的觸摸事件
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                ActiveScrollingTextUI(false);
+            }
+        }
+
+    }
+
+    private void SetScrollSpeed()
+    {
+
+        if (scrollingText == null) return;
+
+        if (float.TryParse(InputField_Text_Speed.text, out float scrollSpeed))
+            scrollingText.ScrollSpeed = scrollSpeed;
+
+    }
+
+    private void SetTextColor()
+    {
+
+        if (scrollingText == null) return;
+
+        if (float.TryParse(InputField_Text_Color_R.text, out float r) &&
+        float.TryParse(InputField_Text_Color_G.text, out float g) &&
+        float.TryParse(InputField_Text_Color_B.text, out float b) &&
+        float.TryParse(InputField_Text_Color_A.text, out float a))
+        {
+            scrollingText._originalTextMesh.color = new Color(r, g, b, a);
+        }
+
+    }
+
+    public void ActiveScrollingTextUI(bool isActive)
+    {
+
+        Scrolling_Text_Group.SetActive(isActive);
+        Settings.SetActive(!isActive);
+
+        if(isActive)
+            fullScreen_ScrollingText.EnableKeyword("_DOTON");
+        else
+            fullScreen_ScrollingText.DisableKeyword("_DOTON");
+
+    }
+
+
+}
--- Assets/Scripts/ScrollingText.cs
+++ Assets/Scripts/ScrollingText.cs
@@ -1,67 +1,178 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(Image))]
+// 1. add this component to a RectTransform that will act as the mask
+// 2. add a RectMask2D Component to this GameObject and set the pivot of the RectTransform to the left
+// 3. create a child TextMeshProUGUI that will act as the original Text and asign it
+
 public class ScrollingText : MonoBehaviour
 {
 
-	#region -- 資源參考區 --
+    #region -- �귽�ѦҰ� --
 
-	public TMP_InputField InputField_Text_Color_R;
-    public TMP_InputField InputField_Text_Color_G;
-    public TMP_InputField InputField_Text_Color_B;
-    public TMP_InputField InputField_Text_Color_A;
-	public TMP_InputField InputField_Text_Speed;
+    public TextMeshProUGUI _originalTextMesh;
+
+    public float ScrollSpeed { get; set; } = 2;
 
     #endregion
 
-    #region -- 變數參考區 --
+    #region -- �ܼưѦҰ� --
 
-    private Image Image_ScrollingText;
-	private Material Material_ScrollingText;
+    RectTransform _originalRect;
+    float _textPreferredWidth;
+    LinkedList<RectTransform> _textTransforms;
 
-    float r, g, b, a, speed;
+    /// <summary>
+    /// if you want spaces at the end -> put \t at the end, it will be ignored, but spaces will work
+    /// </summary>
+    public TextMeshProUGUI TextMesh => _originalTextMesh;
 
     #endregion
 
-    #region -- 初始化/運作 --
+    #region -- ��l��/�B�@ --
 
-    private void Awake()
-	{
+    private void OnEnable()
+    {
 
-        Image_ScrollingText = this.GetComponent<Image>();
-        Material_ScrollingText = Image_ScrollingText.material;
+        if (_originalTextMesh == null)
+        {
+            enabled = false;
+            Debug.LogWarning("Scrolling text was disabled, because no original Text was asigned.");
+            return;
+        }
+
+        _textTransforms = new();
+
+        _originalRect = _originalTextMesh.GetComponent<RectTransform>();
+        _textPreferredWidth = _originalTextMesh.preferredWidth;
+
+        if (_textPreferredWidth == 0)
+        {
+            enabled = false;
+            Debug.LogWarning("Scrolling text was disabled, because there is no text.");
+        }
+
+        _textTransforms.AddFirst(_originalRect);
+        CreateClones();
 
     }
 
-	private void Update()
-	{
-
-        if (float.TryParse(InputField_Text_Color_R.text, out r) &&
-        float.TryParse(InputField_Text_Color_G.text, out g) &&
-        float.TryParse(InputField_Text_Color_B.text, out b) &&
-        float.TryParse(InputField_Text_Color_A.text, out a) &&
-        float.TryParse(InputField_Text_Speed.text, out speed))
-        {
-            Image_ScrollingText.color = new Color(r, g, b, a);
-            Material_ScrollingText.SetFloat("_MoveSpeed", speed);
-        }
-        else
-        {
-            Debug.LogError("One or more inputs are invalid.");
-        }
-
-
+    void Update()
+    {
+        MoveTransforms();
     }
 
-	#endregion
-	
-	#region -- 方法參考區 --
+    private void OnDisable()
+    {
 
-	#endregion
-	
+        foreach (var textTransform in _textTransforms)
+        {
+
+            if(textTransform == _originalRect)
+                continue;
+
+            Destroy(textTransform.gameObject);
+
+        }
+        
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Creates new clones from <see cref="TextMesh"/>.
+    /// Call directly after modifying the textMesh of the original.
+    /// </summary>
+    public void UpdateClones()
+    {
+        foreach (RectTransform rectTrans in _textTransforms)
+        {
+            if (rectTrans != _originalRect)
+                Destroy(rectTrans.gameObject);
+        }
+
+        _textTransforms.Clear();
+
+        _textTransforms.AddFirst(_originalRect);
+
+        _textPreferredWidth = _originalTextMesh.preferredWidth;
+
+        _originalRect.localPosition = new Vector3(0, _originalRect.localPosition.y, _originalRect.localPosition.z);
+
+        if (_textPreferredWidth == 0)
+        {
+            enabled = false;
+            Debug.LogWarning("Scrolling text was disabled, because there is no text.");
+            return;
+        }
+        else enabled = true;
+
+        CreateClones();
+    }
+
+    void MoveTransforms()
+    {
+        float distance = ScrollSpeed * 30 * Time.deltaTime;
+
+        foreach (RectTransform transform in _textTransforms)
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.x -= distance;
+            transform.localPosition = newPos;
+        }
+
+        CheckIfLeftMostTransformLeftMask();
+    }
+
+    void CheckIfLeftMostTransformLeftMask()
+    {
+        RectTransform rectTransform = _textTransforms.First.Value;
+
+        if (rectTransform.localPosition.x + _textPreferredWidth <= 0)
+            ReattachFirstTransformAtTheEnd();
+    }
+
+    void AttachTransformAtTheEnd(RectTransform rectTransform)
+    {
+        float lastTransPosX = _textTransforms.Last.Value.localPosition.x;
+
+        Vector3 newPos = rectTransform.localPosition;
+        newPos.x = lastTransPosX + _textPreferredWidth;
+
+        rectTransform.localPosition = newPos;
+    }
+
+    void ReattachFirstTransformAtTheEnd()
+    {
+        float lastTransPosX = _textTransforms.Last.Value.localPosition.x;
+
+        LinkedListNode<RectTransform> node = _textTransforms.First;
+
+        Vector3 newPos = node.Value.localPosition;
+        newPos.x = lastTransPosX + _textPreferredWidth;
+        node.Value.localPosition = newPos;
+
+        _textTransforms.RemoveFirst();
+        _textTransforms.AddLast(node);
+    }
+
+    void CreateClones()
+    {
+        int clones = CalculateNecessaryClones();
+
+        for (int i = 1; i <= clones; i++)
+        {
+            RectTransform cloneTransform = Instantiate(_originalRect, transform);
+            AttachTransformAtTheEnd(cloneTransform);
+            _textTransforms.AddLast(cloneTransform);
+        }
+    }
+
+    int CalculateNecessaryClones()
+    {
+        RectTransform maskTransform = GetComponent<RectTransform>();
+        return (int)Mathf.Ceil(maskTransform.rect.width / _textPreferredWidth);
+    }
+
 }
